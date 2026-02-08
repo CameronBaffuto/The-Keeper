c781346a99c0674871cb2432fb0be948d7cab2ae
cameronbaffuto <cam@cambaffuto.com>
2026-02-08 16:17:17 -0500

    clean up

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import keeperLogo from '@/assets/thekeeper.png'
+const name = 'The Keeper'
+</script>
+
+<template>
+  <div class="flex flex-col justify-start gap-4">
+    <h1 class="text-3xl font-bold">{{ name }}</h1>
+    <img :src="keeperLogo" class="w-32" />
+  </div>
+</template>
