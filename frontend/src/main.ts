6a2aafa7551afa34935684cf0e3cd26f65dde418
cameronbaffuto <cam@cambaffuto.com>
2026-02-09 19:29:38 -0500

    add packages

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(VueQueryPlugin)
+app.mount('#app')
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
 import keeperLogo from '@/assets/thekeeperLogo.png'
 const name = 'The Keeper'
 </script>
@@ -7,5 +8,12 @@ const name = 'The Keeper'
   <div class="flex flex-col justify-start gap-4">
     <h1 class="text-3xl font-bold">{{ name }}</h1>
     <img :src="keeperLogo" class="w-32" />
+    <Button>Primary Button</Button> 
+    <Button variant="outline">
+        Default
+    </Button>
+    <Button variant="destructive">
+      Destructive
+    </Button>
   </div>
 </template>
